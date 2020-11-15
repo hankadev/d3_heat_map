2635d32235d50780176987e70298772ebad4623f
Hana Klingová <hana.klingova@gmail.com>
2020-11-15 16:57:42 +0100

    publish project

---
+++ README.md
@@ -0,0 +1,36 @@
+# Visualize Data with a Heat Map
+
+This is my implementation of [Free Code Camp's](https://www.freecodecamp.org/) Visualize Data with a Heat Map project.
+
+[view in browser](https://hanny21.github.io/d3_heat_map/)
+
+## Objective
+
+Built an app that is functionally similar to [this](https://codepen.io/freeCodeCamp/full/JEXgeY/).
+Fulfill the below user stories. Give it your own personal style. You can use HTML, JavaScript, CSS, and the D3 svg-based visualization library.
+
+Dataset provided by freeCodeCamp can be found [here](https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json).
+
+User stories:
+
+- **User Story #1:** My heat map should have a title with a corresponding `id="title"`.
+- **User Story #2:** My heat map should have a description with a corresponding `id="description"`.
+- **User Story #3:** My heat map should have an x-axis with a corresponding `id="x-axis`".
+- **User Story #4:** My heat map should have a y-axis with a corresponding `id="y-axis`".
+- **User Story #5:** My heat map should have `rect` elements with a `class="cell"` that represent the data.
+- **User Story #6:** There should be at least 4 different fill colors used for the cells.
+- **User Story #7:** Each cell will have the properties `data-month`, `data-year`, `data-temp` containing their corresponding month, year, and temperature values.
+- **User Story #8:** The `data-month`, `data-year` of each cell should be within the range of the data.
+- **User Story #9:** My heat map should have cells that align with the corresponding month on the y-axis.
+- **User Story #10:** My heat map should have cells that align with the corresponding year on the x-axis.
+- **User Story #11:** My heat map should have multiple tick labels on the y-axis with the full month name.
+- **User Story #12:** My heat map should have multiple tick labels on the x-axis with the years between 1754 and 2015.
+- **User Story #13:** My heat map should have a legend with a corresponding `id="legend"`.
+- **User Story #14:** My legend should contain `rect` elements.
+- **User Story #15:** The `rect` elements in the legend should use at least 4 different fill colors.
+- **User Story #16:** I can mouse over an area and see a tooltip with a corresponding `id="tooltip"` which displays more information about the area.
+- **User Story #17:** My tooltip should have a `data-year` property that corresponds to the `data-year` of the active area.
+
+## Dependencies
+
+Web browser: Google Chrome (recommended)
---
+++ js/script.js
@@ -0,0 +1,163 @@
+/* global d3 */
+(function () {
+  'use strict';
+  const colors = ['#ff6600', '#ff944d', '#ffa64d', '#ffcc66', '#ffff99', '#e6e6ff', '#ccccff', '#9999ff'];
+  const texts = ['over 12°C', '10-12°C', '9-10°C', '8-9°C', '7-8°C', '6-7°C', '4-6°C', 'below 4°C']
+
+  function getCellColor(baseTemperature, variance) {
+    var temp = baseTemperature + variance;
+    if (temp >= 12) {
+      return colors[0];
+    }
+    if (temp >= 10) {
+      return colors[1];
+    }
+    if (temp >= 9) {
+      return colors[2];
+    }
+    if (temp >= 8) {
+      return colors[3];
+    }
+    if (temp >= 7) {
+      return colors[4];
+    }
+    if (temp >= 6) {
+      return colors[5];
+    }
+    if (temp >= 4) {
+      return colors[6];
+    }
+    else {
+      return colors[7];
+    }
+  }
+
+  function showTooltip(item, baseTemperature) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return months[item.month - 1] + ' ' + item.year + '<br />' + (baseTemperature + item.variance).toFixed(2) + '°C';
+  }
+
+  const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json';
+  d3.json(url).then(function (data) {
+    if (data && data.baseTemperature && data.monthlyVariance) {
+      const baseTemperature = data.baseTemperature;
+      const dataset = data.monthlyVariance;
+
+      var descriptionEl = document.getElementById('description');
+      descriptionEl.textContent = dataset[0].year + ' - ' + dataset[dataset.length - 1].year + ': base temperature ' + baseTemperature + '°C';
+
+      const width = 900;
+      const height = 500;
+      const paddingW = 80;
+      const paddingH = 40;
+
+      const minDate = new Date(0, 0, 0, 0, 0, 0, 0);
+      const maxDate = new Date(0, 12, 0, 0, 0, 0, 0);
+      const minYear = d3.min(dataset, d => d.year);
+      const maxYear = d3.max(dataset, d => d.year);
+
+      const cellHeight = (height - 2 * paddingH) / 12;
+      const cellWidth = (width - 2 * paddingW) / (maxYear - minYear);
+
+      const xScale =
+        d3.scaleLinear()
+          .range([paddingW, width - paddingW])
+          .domain([new Date(minYear), new Date(maxYear)]);
+
+      const yScale =
+        d3.scaleTime()
+          .range([paddingH, height - paddingH])
+          .domain([new Date(minDate), new Date(maxDate)]);
+
+      // create svg element and append it to body
+      const svg =
+        d3.select('#canvas')
+          .append('svg')
+          .attr('width', width)
+          .attr('height', height);
+
+      // add tooltip 
+      const tooltip =
+        d3.select('#canvas')
+          .append('div')
+          .attr('id', 'tooltip');
+
+      // add axis to svg canvas
+      const xAxis =
+        d3.axisBottom(xScale)
+          .tickFormat(d3.format('d'));
+
+      svg.append('g')
+        .attr('id', 'x-axis')
+        .attr('transform', 'translate(0,' + (height - paddingH) + ')')
+        .call(xAxis);
+
+      const yAxis =
+        d3.axisLeft(yScale)
+          .tickFormat(d3.timeFormat('%B'));
+
+      svg.append('g')
+        .attr('id', 'y-axis')
+        .attr('transform', 'translate(' + paddingW + ',0)')
+        .call(yAxis);
+
+      // add cells to the graph
+      svg.selectAll('rect')
+        .data(dataset)
+        .enter()
+        .append('rect')
+        .attr('class', 'cell')
+        .attr('x', d => xScale(d.year))
+        .attr('y', d => yScale(new Date(0, d.month - 1, 0, 0, 0, 0, 0)))
+        .attr('width', cellWidth)
+        .attr('height', cellHeight)
+        .attr('fill', cell => getCellColor(baseTemperature, cell.variance))
+        .attr('data-year', d => d.year)
+        .attr('data-month', d => d.month - 1)
+        .attr('data-temp', d => baseTemperature + d.variance)
+        .on('mouseover', d => {
+          tooltip.attr('data-year', d.year)
+            .html(showTooltip(d, baseTemperature))
+            .style('top', (d3.event.pageY - 60) + 'px')
+            .style('left', (d3.event.pageX) + 'px')
+            .style('visibility', 'visible');
+        })
+        .on('mouseout', () => {
+          tooltip.style('visibility', 'hidden');
+        });
+
+      // create legend
+      const legendRectWidht = 80;
+
+      const legend =
+        d3.select('#legend')
+          .append('svg')
+          .attr('width', width)
+          .attr('height', 50)
+          .append("g")
+          .attr('transform', 'translate(' + paddingW + ', 0)')
+          .attr('id', 'legend')
+
+      legend.selectAll('rect')
+        .data(colors.reverse())
+        .enter()
+        .append('rect')
+        .attr('x', (item, index) => index * legendRectWidht)
+        .attr('y', 0)
+        .attr('width', legendRectWidht)
+        .attr('height', 20)
+        .attr('fill', c => c);
+
+      legend.selectAll('text')
+        .data(texts.reverse())
+        .enter()
+        .append('text')
+        .attr('x', (item, index) => index * legendRectWidht)
+        .attr('y', 35)
+        .attr('width', 0)
+        .attr('height', 20)
+        .text(t => t)
+    }
+
+  });
+}());
